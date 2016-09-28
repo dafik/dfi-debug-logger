5fed4588fa69b8cd4f403ea17e9e36681efb96e9
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-09-28 15:11:49 +0200

    typescript

---
+++ debugLogger.d.ts
@@ -0,0 +1,13 @@
+import * as debug from "debug";
+export declare class DebugLogger {
+    name: string;
+    private _loggers;
+    constructor(name: string);
+    getLogger(type: string): debug.IDebugger;
+    trace(formatter: any, ...args: any[]): void;
+    debug(formatter: any, ...args: any[]): void;
+    info(formatter: any, ...args: any[]): void;
+    warn(formatter: any, ...args: any[]): void;
+    error(formatter: any, ...args: any[]): void;
+    fatal(formatter: any, ...args: any[]): void;
+}
