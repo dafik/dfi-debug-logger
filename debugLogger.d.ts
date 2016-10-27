27fac0b677685cc313684ae02dcc35249bc1312c
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-10-27 10:55:13 +0200

    typing fix

--- debugLogger.d.ts
+++ debugLogger.d.ts
@@ -1,20 +1,37 @@
 import * as debug from "debug";
-declare class DebugLogger {
-    name: string;
-    private _loggers;
-    constructor(name: string);
-    getLogger(type: string): debug.IDebugger;
-    trace(formatter: any, ...args: any[]): void;
-    debug(formatter: any, ...args: any[]): void;
-    info(formatter: any, ...args: any[]): void;
-    warn(formatter: any, ...args: any[]): void;
-    error(formatter: any, ...args: any[]): void;
-    fatal(formatter: any, ...args: any[]): void;
-    isTraceEnabled(): boolean;
-    isDebugEnabled(): boolean;
-    isInfoEnabled(): boolean;
-    isWarnEnabled(): boolean;
-    isErrorEnabled(): boolean;
-    isFatalEnabled(): boolean;
+
+declare module "local-dfi-debug-logger" {
+    class DebugLogger {
+        name: string;
+        private _loggers;
+
+        constructor(name: string);
+
+        getLogger(type: string): debug.IDebugger;
+
+        trace(formatter: any, ...args: any[]): void;
+
+        debug(formatter: any, ...args: any[]): void;
+
+        info(formatter: any, ...args: any[]): void;
+
+        warn(formatter: any, ...args: any[]): void;
+
+        error(formatter: any, ...args: any[]): void;
+
+        fatal(formatter: any, ...args: any[]): void;
+
+        isTraceEnabled(): boolean;
+
+        isDebugEnabled(): boolean;
+
+        isInfoEnabled(): boolean;
+
+        isWarnEnabled(): boolean;
+
+        isErrorEnabled(): boolean;
+
+        isFatalEnabled(): boolean;
+    }
+    export = DebugLogger;
 }
-export = DebugLogger;
