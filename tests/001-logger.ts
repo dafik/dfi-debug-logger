98da8714d4a4f4f5e861f21b7481de22c0b6fa8b
z.wieczorek <zbyszek.wieczorek@gmail.com>
2016-11-03 12:40:53 +0100

    typing fix + test

--- tests/001-logger.ts
+++ tests/001-logger.ts
@@ -1,56 +1,78 @@
-"use strict";
-const assert = require("assert");
-const DebugLogger = require("../debugLogger");
+import assert = require("assert");
+import DebugLogger = require("../debugLogger");
+
 describe("object", () => {
+
     function onLoggerDisabled(done) {
         let loggerName = "testLogger";
         let logger = new DebugLogger(loggerName);
+
         assert.equal(logger.name, loggerName);
+
         assert.equal(logger.isTraceEnabled(), false);
         assert.equal(logger.isDebugEnabled(), false);
         assert.equal(logger.isInfoEnabled(), false);
         assert.equal(logger.isWarnEnabled(), false);
         assert.equal(logger.isErrorEnabled(), false);
         assert.equal(logger.isFatalEnabled(), false);
+
         done();
     }
+
     function onLoggerEnabled(done) {
         let loggerName = "testLogger";
+
         process.env.DEBUG = loggerName + "*";
+
         let path = process.cwd() + "/node_modules/debug/debug.js";
+
         delete require.cache[require.resolve("debug")];
         delete require.cache[path];
         delete require.cache[require.resolve("../debugLogger")];
-        let DebugLogger1 = require("../debugLogger");
-        let logger = new DebugLogger1(loggerName);
+
+        let debugLogger1 = require("../debugLogger");
+
+        let logger = new debugLogger1(loggerName);
+
         assert.equal(logger.name, loggerName);
+
         assert.equal(logger.isTraceEnabled(), true);
         assert.equal(logger.isDebugEnabled(), true);
         assert.equal(logger.isInfoEnabled(), true);
         assert.equal(logger.isWarnEnabled(), true);
         assert.equal(logger.isErrorEnabled(), true);
         assert.equal(logger.isFatalEnabled(), true);
+
         done();
     }
+
     function onLoggerTest(done) {
         function logFn(message) {
             assert.ok(message.match(loggerMessage) !== null);
             done();
         }
+
         let loggerName = "testLogger";
         let loggerMessage = "testMessage";
+
         process.env.DEBUG = loggerName + "*";
+
         let path = process.cwd() + "/node_modules/debug/debug.js";
+
         delete require.cache[require.resolve("debug")];
         delete require.cache[path];
         delete require.cache[require.resolve("../debugLogger")];
-        let DebugLogger1 = require("../debugLogger");
-        let logger = new DebugLogger1(loggerName);
+
+        let debugLogger1 = require("../debugLogger");
+
+        let logger = new debugLogger1(loggerName);
         logger.getLogger("error").log = logFn;
+
         logger.error(loggerMessage);
     }
+
     it("logger disabled", onLoggerDisabled);
     it("logger enabled", onLoggerEnabled);
     it("logger test", onLoggerTest);
+
 });
-//# sourceMappingURL=test.js.map
