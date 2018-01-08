53a2302db08b8b1af24e3a7c5be20e3c24ff8c29
Zbyszek Wieczorek <zbyszek.wieczorek@gmail.com>
2018-01-08 08:48:06 +0100

    remove js and map from repo

--- browser.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const log = (...args) => {
-    console.log.apply(args);
-};
-exports.default = log;
-//# sourceMappingURL=browser.js.map
--- node.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const util = require("util");
-const log = (...args) => {
-    process.stdout.write(util.format.apply(util, args) + "\n");
-};
-exports.default = log;
-//# sourceMappingURL=node.js.map
